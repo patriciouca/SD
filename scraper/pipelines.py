1c22f2692160d523f6bfe9aefc215cbc3bc1b804
patricio <patricio.fernandezflorez@alum.uca.es>
2019-04-21 02:21:36 +0200

    inicio

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+from django.utils import timezone
+
 
 
 class AmazontrackPipeline(object):
     def process_item(self, item, spider):
-        f = open("datosProducto.txt", "a")
-        f.write(item['nombre']+" "+item['precio'])
-        f.close()
+        ruta = 'datos/'+"nombre"+'.csv'
+        print(ruta)
+        #f = open("datosProducto.txt", "a")
+        #f.write(item['nombre']+" "+item['precio'])
+        #f.close()
+        with open(ruta, 'w', newline='') as csvfile:
+            fieldnames = ['fecha', 'precio']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'fecha': datetime.datetime.now().time(), 'precio': item['precio']})
+        '''
+        try:
+            #fh = open('/datos/'+item['titulo']+'.csv', 'r')
+            #with open('/datos/'+item['titulo']+'.csv', 'a', newline='') as csvfile:
+                #fieldnames = ['fecha', 'precio']
+                #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                #writer.writeheader()
+                #writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})
+        except FileNotFoundError:
+            with open('/datos/'+"nombre"+'.csv', 'w' ,newline='') as csvfile:
+                fieldnames = ['fecha', 'precio']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})'''
         return item
