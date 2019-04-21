43d629665d87b07f0e63fde24eb00d7142b88672
patricio <patricio.fernandezflorez@alum.uca.es>
2019-04-21 02:30:33 +0200

    inicio

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -12,23 +12,18 @@ class AmazontrackPipeline(object):
         #f = open("datosProducto.txt", "a")
         #f.write(item['nombre']+" "+item['precio'])
         #f.close()
-        with open(ruta, 'w', newline='') as csvfile:
-            fieldnames = ['fecha', 'precio']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({'fecha': datetime.datetime.now().time(), 'precio': item['precio']})
-        '''
+
         try:
-            #fh = open('/datos/'+item['titulo']+'.csv', 'r')
-            #with open('/datos/'+item['titulo']+'.csv', 'a', newline='') as csvfile:
-                #fieldnames = ['fecha', 'precio']
-                #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                #writer.writeheader()
-                #writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})
+            with open(ruta, 'w', newline='') as csvfile:
+                fieldnames = ['fecha', 'precio']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})
+
         except FileNotFoundError:
             with open('/datos/'+"nombre"+'.csv', 'w' ,newline='') as csvfile:
                 fieldnames = ['fecha', 'precio']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
-                writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})'''
+                writer.writerow({'fecha': timezone.now(), 'precio': item['precio']})
         return item
