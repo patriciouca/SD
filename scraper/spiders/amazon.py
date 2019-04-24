51af61cde4a6d2da4354979496bbeac1ffe690c4
patricio <patricio.fernandezflorez@alum.uca.es>
2019-04-24 17:14:43 +0200

    scrapy

--- scraper/spiders/amazon.py
+++ scraper/spiders/amazon.py
@@ -18,9 +18,16 @@ class AmazonSpider(scrapy.Spider):
         ml_item = AmazonItem()
 
         titulo = response.xpath('//span[@id="productTitle"]/text()').get()
-        titulo= titulo.rstrip().lstrip()
         precio = response.xpath('//span[@id="priceblock_ourprice"]/text()').get()
-        precio=precio.replace('EUR', "", )
+        titulo= titulo.rstrip().lstrip()
+        if not precio:
+            precio = response.xpath('//span[@class="a-color-price"]/text()').get()
+
+        precio = str(precio)
+        if "EUR" in precio:
+            precio=precio.replace('EUR', "", )
+        if "$" in precio:
+            precio = precio.replace('$', "", )
         print(titulo)
         print(precio)
         ml_item['nombre'] = titulo
